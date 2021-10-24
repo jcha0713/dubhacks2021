eb659ae645fa1811a532d477f1f2030ad48260b4
Ryan Carroll <rcarro@uw.edu>
2021-10-23 19:48:33 -0700

    Add about page and corrects lists

--- src/components/AboutPage.js
+++ src/components/AboutPage.js
@@ -4,7 +4,35 @@ import React from 'react';
 export default function AboutPage() {
     return (
         <div>
-            <h1>About Page View</h1>
+            <div>
+                <h1>About Page View</h1>
+            </div>
+
+            <div>
+                <h3>Why did we choose to do this project</h3>
+                <p>
+                When COVID-19 threatened our community we looked towards technology and increased our online presence to connect us to our communities and resources. As our online presence increases to help us navigate our new normal, it comes with understanding there are risks involved in doing so. The internet can be a not-so-safe place and it’s important to understand the risks involved as we continue to rely on technology. We chose to implement a page asking users to accept a privacy policy because a study by PEW research found “about eight-in-ten Americans say they are asked to agree to a privacy policy at least monthly, including one-quarter of which who say this happens almost every day” therefor this application is relevant to most peoples lives (PEW). Furthermore, people may consent to the use of their personal data, without being sufficiently aware or informed of the nature and extent of potential implications. Our goal of this project was to get people thinking about how their increased online presence comes at a cost. We aren’t saying everyone should go change all their privacy setting immediately, but we want to create a more conscious community of technology users. A community where people can start to formulate opinions as to where they lie on the issue of personal data collection and cyber security.
+                </p>
+
+                <h3>The future of personal data collection</h3>
+                
+                    <ul><a href="https://interestingengineering.com/what-data-are-voice-assistants-collecting-and-how-to-protect-yourself">Smart Speakers</a></ul>
+                    <ul><a href="https://www.wired.com/2016/01/under-armour-healthbox/">Sensor Embedded Clothing</a></ul>
+                    <ul><a href="https://www.wired.com/story/apple-watch-heart-monitoring-pros-and-cons/">Wearable Health Monitors</a></ul>
+                    <ul><a href="https://www.orlandosentinel.com/news/breaking-news/os-amazon-orlando-police-cameras-downtown-20180524-story.html">Facial regognition enabled surveillance cameras</a></ul>
+                
+                <h3>Cyber Security ethics to think about</h3>
+                <ul>Should colleges be permitted to digitally track their teenage applicants?</ul>
+                <ul>Do we really want health insurance companies monitoring our Instagram posts?</ul>
+                <ul>As our lives become more dependent on technology are we ready to be observed and recorded to an unfathomable extent?</ul>
+
+
+            </div>
+
+
+
+
         </div>
+        
     )
 }
--- src/components/BootcampPage1.js
+++ src/components/BootcampPage1.js
@@ -17,16 +17,16 @@ export default function BootcampPage1() {
                 <p>If you don’t read the entire terms of use its best to assume your personal information is being collected</p>
                 <h3>What kind of data is being collected?</h3>
                 <ol>
-                    <p><strong>1. Your Location</strong></p>
+                    <li><strong>Your Location</strong></li>
                         <ul>
                             By allowing companies access to this it can help companies and shops pinpoint our location allowing marketers to be notified when we walk past a store and how much time is spent there. This can add to our shopping or browsing experience but is giving companies an unfiltered look at our lives and the places we visit.
                         </ul>
-                    <p>2. Addresses of which sites you visit/have visited (as well as where you click and your mouse strokes)</p>
+                    <li><strong>Addresses of which sites you visit/have visited (as well as where you click and your mouse strokes)</strong></li>
                         <ul>
                             This feature pinpoints your interests and also helps companies further understand usability statistics.
                         </ul>
-                    <p>3. Usernames and Passwords</p>
-                    <p>4. Technical configurations of your device</p>
+                    <li><strong>Usernames and Passwords</strong></li>
+                    <li><strong>Technical configurations of your device</strong></li>
                 </ol>
                     <p></p>
                 {/* When clicking on button should bring you to next page */}
